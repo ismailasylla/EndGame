2882a045c59b70fc65eb23214d478073561b4079
Sylla <ismailasyllaa@gmail.com>
2024-02-07 22:36:30 +0400

    implement query variables to get single items

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ import db from './_db.js';
 // resolvers
 const resolvers = {
   Query: {
-    games() {
-      return db.games;
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
     },
     authors() {
       return db.authors;
     },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
     reviews() {
       return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
     },
   },
 };
