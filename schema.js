02bed83c4bd7a2706cd879ccf1fd3d42a2f50208
Sylla <ismailasyllaa@gmail.com>
2024-02-08 14:38:29 +0400

    Add relationship for related data in game and author reviews

--- schema.js
+++ schema.js
@@ -1,18 +1,22 @@
 export const typeDefs = `#graphql
   type Game {
-    id: ID!,
-    title: String!,
-    platform: [String]!
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
   }
   type Review {
     id:ID!,
-    rating: Int!,
+    rating: Int!
     content: String!
+    game: Game!
+    author: Author!
   }
   type Author {
-    id: ID!,
-    name: String!,
-    verified: Boolean! 
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]  
   }
   type Query {
     games: [Game]
